ef877ed010bfea12da5709298122a75b486fca36
RamBanjo <rambanjo@gmail.com>
2022-06-02 17:36:24 +0700

    RewriteRule compatibility added

---
+++ TestCompo_Rewrite_Requirements.py
@@ -0,0 +1,7 @@
+#Testing Requirement for Rewrite Rules
+
+from components.RewriteRules import RewriteRule
+from components.StoryGraph import StoryGraph
+
+story_graph = StoryGraph
+my_rules = RewriteRule
--- components/RewriteRules.py
+++ components/RewriteRules.py
@@ -17,23 +17,44 @@ class RewriteRule:
     
     Story Change
     Relationship Change (already included in story_changes and story_condition thanks to timesteps coming pre-included with world states)
+
+    bias range example:
+    (it's a dict with the bias as the key and the range)
+    {"lawbias": (0, 100)}
     '''
     
-    def __init__(self, story_condition: StoryGraph, story_change: StoryGraph, dummychar, name="", required_tags=[], unwanted_tags=[], bias=None):
+    def __init__(self, story_condition: StoryGraph, story_change: StoryGraph, dummychar, name="", required_tags=[], unwanted_tags=[], bias_range=None):
         self.rule_name = name
         self.story_condition = story_condition
         self.story_change = story_change
         self.dummychar = dummychar
+        self.required_tags = required_tags
+        self.unwanted_tags = unwanted_tags
+        self.bias_range = bias_range
 
     def check_character_compatibility(self, character_node):
+
+        compatibility = True
+
         #TODO: Check if the character contains tags in Required Tags (not compatible if false)
 
+        for tag in self.required_tags.values():
+            compatibility = compatibility and tag in character_node.tags.values()
+
         #TODO: Check if character contains tags in Unwanted Tags (not compatible if true)
+
+        for tag in self.unwanted_tags.values():
+            compatibility = compatibility and tag not in character_node.tags.values()
         
         #TODO: Check if character's bias is within the acceptable range (not compatible if false)
-
+        if self.bias_range is not None:
+            for bias in self.bias_range:
+                char_bias_value = character_node.biases[bias]
+                compatibility = compatibility and char_bias_value >= self.bias_range[bias][0]
+                compatibility = compatibility and char_bias_value <= self.bias_range[bias][1]
+                
         #If the character passes all three tests, then return true. Otherwise, return false
-        return True
+        return compatibility
 '''        
 Hey wait a second Ram, this method of rewriting rules might not work for our purpose
 Consider: This method will not preserve the previous state of the story map
