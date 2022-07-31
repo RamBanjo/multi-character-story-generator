6ed9938752c66994f29e9e84c4e79488774e6397
RamBanjo <rambanjo@gmail.com>
2022-08-01 06:12:38 +0700

    Updated Todo List

--- TestCompo_WorldState.py
+++ TestCompo_WorldState.py
@@ -4,15 +4,9 @@ We're gonna going to have to test a lot of things!
 
 First off:
 - Location Dependent World State
-- Multiple actions in one timestep
-- Actually, we also need to bring back timesteps
-
-Timestep
-- Each timestep contains multiple story parts and its own World State
+- TODO: Draft how it would look like on paper
 
 StoryGraph
 - Rewrite it to support adding multiple nodes
-
-StoryNode
-- Add a "Timestep" property to it, to prevent events from different timesteps from being used together
+- TODO: Draft the before and after of how nodes should look like on paper
 '''
