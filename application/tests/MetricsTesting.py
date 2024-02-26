d2bef13451920c39330757a91062bbaaa339d5f2
RamBanjo <rambanjo@gmail.com>
2024-02-26 13:13:52 +0700

    created metrics testing file

--- application/tests/MetricsTesting.py
+++ application/tests/MetricsTesting.py
@@ -7,6 +7,34 @@ from components.StoryObjects import *
 from components.WorldState import *
 from components.StoryGraphTwoWS import *
 
+alice = CharacterNode(name="Alice", biases={"lawbias":50, "moralbias":50})
+bob = CharacterNode(name="Bob", biases={"lawbias":0, "moralbias":-50})
+
+somewhere = LocationNode(name="Somewhere")
+
+test_ws = WorldState(name="Test WS", objectnodes=[alice, bob, somewhere])
+test_ws.connect(from_node=somewhere, edge_name="holds", to_node=alice)
+test_ws.connect(from_node=somewhere, edge_name="holds", to_node=bob)
+
+action_a = StoryNode(name="Action A")
+costly_action_b = StoryNode(name="Action B", tags={"Type":"Placeholder","costly":True})
+important_action_c = StoryNode(name="Action C", tags={"Type":"Placeholder","important_actiion":True})
+action_d = StoryNode(name="Action D")
+action_e = StoryNode(name="Action E")
+action_f = StoryNode(name="Action F")
+
+test_sg_cost = StoryGraph(name="Test Costly SG", character_objects=[alice, bob], starting_ws=test_ws)
+test_sg_unique = StoryGraph(name="Test Unique SG", character_objects=[alice, bob], starting_ws=test_ws)
+test_sg_jointy = StoryGraph(name="Test Jointy SG", character_objects=[alice, bob], starting_ws=test_ws)
+test_sg_prefer = StoryGraph(name="Test Prefer SG", character_objects=[alice, bob], starting_ws=test_ws)
+
+#Alice's storyline has 2 costly actions and is 5 nodes long. The score should be 20.
+test_sg_cost.insert_multiple_parts(part_list=[costly_action_b, costly_action_b, action_a, important_action_c, action_d], character=alice, location_list=[somewhere, somewhere, somewhere, somewhere, somewhere])
+
+#Bob's storyline has no costly actions and is 5 nodes long. The score should be 0.
+
+#Charlie's storyline has only costly actions. The score should be 100.
+
 #Here is how we will want to test our Metrics Functions:
 
 # Testing measuring costs
@@ -26,3 +54,8 @@ from components.StoryGraphTwoWS import *
 # Scenario 2: Test Uniqueness Metric Rule Follow (True and False)
 # Scenario 3: Test Jointability Metric Rule Follow (True and False)
 # Scenario 4: Test Preference Metric Rule Follow (True and False)
+
+#TODO: Literal shower thoughts, but these metrics can only read and calculate what's in the current graph. We will need to make a solution for when there are multiple graphs at play...
+# - Input previous metrics?
+# - Ignore previous graph's metrics?
+# - Only calculate current graph and previous graph's metrics? (The older graphs should not count it's way in the past)
