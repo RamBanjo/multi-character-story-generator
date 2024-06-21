d29c46b1b6df6e5114b97d239b9ef3fbd0a7bb14
Ram <rambanjo@gmail.com>
2024-06-21 21:33:31 +0700

    moved to metric 1

--- application/tests/EvenLittlerRedWorldState.py
+++ application/tests/EvenLittlerRedWorldState.py
@@ -770,22 +770,22 @@ wait_prob_name_dict = {"Grandma":0.5, "Witch":0.5, "Papa Bear":0.5}
 # generated_graph = generate_story_from_starter_graph(init_storygraph=initial_graph, list_of_rules=list_of_rules, required_story_length=5, verbose=True, extra_attempts=-1)
 #Uncomment each block for the desired result
 #No Metrics
-retention = 0
-generated_graph_list = generate_multiple_graphs(initial_graph=initial_graph, list_of_rules=list_of_rules, required_story_length=25, max_storynodes_per_graph=5, verbose=True, extra_attempts=-1, suggested_movement_requirement_list=movement_suggestion, extra_movement_requirement_list=movement_requirement, task_movement_random=True, extra_move_changes=extra_move_changes, charname_extra_prob_dict=wait_prob_name_dict)
-base_folder_name = "no_metric"
+# retention = 0
+# generated_graph_list = generate_multiple_graphs(initial_graph=initial_graph, list_of_rules=list_of_rules, required_story_length=25, max_storynodes_per_graph=5, verbose=True, extra_attempts=-1, suggested_movement_requirement_list=movement_suggestion, extra_movement_requirement_list=movement_requirement, task_movement_random=True, extra_move_changes=extra_move_changes, charname_extra_prob_dict=wait_prob_name_dict)
+# base_folder_name = "no_metric"
 
 # x0 Retention
 retention = 0
-# generated_graph_list = generate_multiple_graphs(initial_graph=initial_graph, list_of_rules=list_of_rules, required_story_length=25, max_storynodes_per_graph=5, verbose=True, extra_attempts=-1, suggested_movement_requirement_list=movement_suggestion, metric_requirements=metric_requirements, extra_movement_requirement_list=movement_requirement, task_movement_random=True, metric_retention=retention, extra_move_changes=extra_move_changes, charname_extra_prob_dict=wait_prob_name_dict, metric_reward=200, metric_penalty=-200)
-# base_folder_name = "x0_metric"
+generated_graph_list = generate_multiple_graphs(initial_graph=initial_graph, list_of_rules=list_of_rules, required_story_length=25, max_storynodes_per_graph=5, verbose=True, extra_attempts=-1, suggested_movement_requirement_list=movement_suggestion, metric_requirements=metric_requirements, extra_movement_requirement_list=movement_requirement, task_movement_random=True, metric_retention=retention, extra_move_changes=extra_move_changes, charname_extra_prob_dict=wait_prob_name_dict, metric_reward=200, metric_penalty=-200)
+base_folder_name = "x0_metric_1"
 
 # x0.5 Retention
-retention = 0.5
+# retention = 0.5
 # generated_graph_list = generate_multiple_graphs(initial_graph=initial_graph, list_of_rules=list_of_rules, required_story_length=25, max_storynodes_per_graph=5, verbose=True, extra_attempts=-1, suggested_movement_requirement_list=movement_suggestion, metric_requirements=metric_requirements, extra_movement_requirement_list=movement_requirement, task_movement_random=True, metric_retention=retention, extra_move_changes=extra_move_changes, charname_extra_prob_dict=wait_prob_name_dict, metric_reward=200, metric_penalty=-200)
 # base_folder_name = "xhalf_metric"
 
 # x1 Retention
-retention = 1
+# retention = 1
 # generated_graph_list = generate_multiple_graphs(initial_graph=initial_graph, list_of_rules=list_of_rules, required_story_length=25, max_storynodes_per_graph=5, verbose=True, extra_attempts=-1, suggested_movement_requirement_list=movement_suggestion, metric_requirements=metric_requirements, extra_movement_requirement_list=movement_requirement, task_movement_random=True, metric_retention=retention, extra_move_changes=extra_move_changes, charname_extra_prob_dict=wait_prob_name_dict, metric_reward=200, metric_penalty=-200)
 # base_folder_name = "x1_metric"
 
